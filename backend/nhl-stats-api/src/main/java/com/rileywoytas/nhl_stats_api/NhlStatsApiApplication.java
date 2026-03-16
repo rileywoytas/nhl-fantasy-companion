5d582ffcc68cdb9e3b21c8ff300d5f41a7f3dc4b
rwoytas <rwoytas@selkirksystems.com>
2026-03-15 18:49:00 -0600

    Generating Spring Boot Project

---
+++ backend/nhl-stats-api/src/main/java/com/rileywoytas/nhl_stats_api/NhlStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rileywoytas.nhl_stats_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlStatsApiApplication.class, args);
+	}
+
+}
---
+++ backend/nhl-stats-api/src/test/java/com/rileywoytas/nhl_stats_api/NhlStatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rileywoytas.nhl_stats_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlStatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
